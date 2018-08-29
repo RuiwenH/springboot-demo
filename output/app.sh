4556f0e5f3d8750f80cac01761c651ad15c13962
RuiwenHuang <fly533@126.com>
2018-08-29 14:27:59 +0800

    使用 nohup ，并将nohup.out输出重定向到/dev/null

--- output/app.sh
+++ output/app.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
-#功能：
+# 功能：
 #1、指定用户运行——未实现
 #2、默认logs目录——可否另外指定？
-#3、JMX监控——待实现，需要启用第二参数来指定
+#3、JMX监控——待实现
 #4、设置JVM参数（指定程序运行名称、时区、内存大小、GC回收器设置、gc日志文件）
 #5、指定配置文件的环境变量
 #6、start 启动时检查程序是否已经启动
-#7、stop 停止程序、stop之前会先dump，kill失败检查，然后用kill -9, 待完善：有可能存在多个server
+#7、stop 停止程序、stop之前会先dump，kill失败检查，待完善：然后用kill -9,有可能存在多个server
 #8、status 查看程序是否运行
 #9、restart 重启
 #10、dump dump系统状态和jvm信息到文件中
 #11、配置文件使用哪种方式更好？
 #12、默认的jvm配置
 #13、JMX支持——待实现
-#14、增加远程调试接口  ,如何使用及场景问题https://blog.csdn.net/u014001866/article/details/53993292，https://blog.csdn.net/u011008029/article/details/50586793
-#待补充端口占用检查
+#14、增加远程调试接口
+# 待补充端口占用检查
 
 #使用：
 #1、上传.sh脚本到服务器中,授权脚本执行权限sudo chmod 755 ./app.sh
-#2、启动示例 ./app.sh start 
-#3、重启示例 ./app.sh restart 
-#4、停止示例 ./app.sh stop 
-#5、参数$0 用于指定
+#2、启动示例：./app.sh start 
+#3、重启示例：./app.sh restart 
+#4、停止示例：./app.sh stop 
+#5、其他示例：./app.sh restart pro debug
 
+echo "参数：p0=$0,p1=$1,p2=$2,p3=$3"
+## p0=./app.sh,p1=restart,p2=pro,p3=debug
 
-ENV=$1
-RUNNING_USER=reven
+# 变量
+RUNNING_USER=reven # 指定运行用户
+SERVER_NAME=springboot-demo # jar的名字
+# 项目中日志地址
+LOG_PATH=/logs/springboot_demo_info.log
+# 部署启动指定的配置文件
+DEPLOY_CONFIG=/deploy_config/demo_deploy.yml
+# 远程调试端口
+XDEBUG_ADDRESS=8000
+
+ENV=$2 #指定启动的环境profile，暂时未使用
+typeset -l Xdebug  #转换为小写
+Xdebug=$3 #参数为debug表示启用远程调试监听
+
 ADATE=`date +%Y%m%d%H%M%S`
-SERVER_NAME=springboot-demo
 APP_HOME=`pwd`
-#项目中日志地址
-LOG_PATH=$APP_HOME/logs/springboot_demo_info.log
-
 
 dirname $0|grep "^/" >/dev/null
 if [ $? -eq 0 ];then
@@ -52,15 +62,18 @@ fi
 
 if [ -z "$LOG_PATH"  ];then
     LOG_PATH=$APP_HOME/logs/$SERVER_NAME.out
+else
+    LOG_PATH="$APP_HOME$LOG_PATH"
 fi
 GC_LOG_PATH=$APP_HOME/logs/gclog/gc-$SERVER_NAME-$ADATE.log
 #JMX监控需用到
 JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1091 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 #JVM参数
 #-Djeesuite.configcenter.profile=$ENV
-JVM_OPTS="-Dname=$SERVER_NAME -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Shanghai -Xms1024M -Xmx1024M  -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:$GC_LOG_PATH -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
+JVM_OPTS="-Dname=$SERVER_NAME -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Shanghai -Xms1024M"
+JVM_OPTS=" $JVM_OPTS -Xmx1024M  -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -Xloggc:$GC_LOG_PATH -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 JAR_FILE=$SERVER_NAME.jar
-DEBUG_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n"
+DEBUG_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=$XDEBUG_ADDRESS,suspend=n"
 pid=0
 
 
@@ -69,12 +82,14 @@ start(){
   if [ ! -n "$pid" ]; then
   
     #JAVA_CMD=" java -server -jar $JVM_OPTS $JAR_FILE > $LOG_PATH 2>&1 &"
-    JAVA_CMD="-server $DEBUG_OPTS -jar $JVM_OPTS $JAR_FILE --spring.config.location=$APP_HOME/deploy_config/demo_deploy.yml"
-    if [ ! -z "$2"  ];then
-        JAVA_CMD="$JAVA_CMD DEBUG_OPTS"
+    JAVA_CMD="-server -jar $JVM_OPTS $JAR_FILE --spring.config.location=$APP_HOME$DEPLOY_CONFIG"
+    
+    if [ "$Xdebug" = "debug"  ];then
+        JAVA_CMD="$JAVA_CMD $DEBUG_OPTS"
     fi
+    echo "JAVA_CMD=$JAVA_CMD"
     #su - $RUNNING_USER -c "$JAVA_CMD"
-    java $JAVA_CMD &
+    nohup java $JAVA_CMD >/dev/null 2>&1 &
     echo "---------------------------------"
     echo "启动完成，按CTRL+C退出日志界面即可>>>>>---"
     echo "---------------------------------"
@@ -110,14 +125,6 @@ stop(){
       dump
       echo "$SERVER_NAME stop..."
       kill $pid
-      echo "sleep 5s... "
-      sleep 5s
-      checkpid
-      echo "checkpid again..."
-      if [  -n "$pid" ]; then
-        echo "$SERVER_NAME stop by kill -9 "
-        kill -9 $pid
-      fi
     fi 
 }
 
