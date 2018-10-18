f8ec7a0ae7e32a1609ae610454bd460f5e7813d5
RuiwenHuang <fly533@126.com>
2018-10-18 18:47:50 +0800

    actuator监控

--- src/main/java/com/reven/controller/DemoController.java
+++ src/main/java/com/reven/controller/DemoController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.reven.controller.common.BaseController;
 import com.reven.controller.common.JxlsExcelView;
