25a8473f561717ac8c545f2be356be11d9bb2e92
revenhuang <fly533@126.com>
2018-12-13 21:15:10 +0800

    合并

--- src/test/java/com/reven/Foo.java
+++ src/test/java/com/reven/Foo.java
@@ -4,7 +4,26 @@ import java.util.HashMap;
 
 public class Foo {
     public static void main(String[] args) {
-        Float.isNaN(1);
-        HashMap map = new HashMap();
+//        Float.isNaN(1);
+//        HashMap map = new HashMap();
+        Integer a=new Integer(128);
+        Integer b=new Integer(128);
+        System.out.println(b.equals(a));
+        System.out.println(a==b);
+        
+        Integer c=new Integer(127);
+        Integer d=new Integer(127);
+        System.out.println(c.equals(d));
+        System.out.println(c==d);
+        
+//        Integer c=128;
+//        int d=128;
+//        System.out.println(c.equals(d));
+//        System.out.println(c==d);
+        
+        String ab="addfg";
+        System.out.println(ab.replace("dd", "DD"));
+        System.out.println(ab.replaceAll("d", "D"));
+        
     }
 }
