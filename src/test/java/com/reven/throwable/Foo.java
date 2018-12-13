3867a5d5fddaebb09ce9514c1c40d49bc239b6d0
revenhuang <fly533@126.com>
2018-12-13 21:26:48 +0800

    是

--- src/test/java/com/reven/throwable/Foo.java
+++ src/test/java/com/reven/throwable/Foo.java
@@ -6,12 +6,6 @@ import java.io.IOException;
 
 public class Foo {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(…);
-                BufferedWriter writer = new BufferedWriter(…)) {// Try-with-resources
-           // do something
-           catch ( IOException | XEception e) {// Multiple catch
-              // Handle it
-           } 
 
     }
 }
